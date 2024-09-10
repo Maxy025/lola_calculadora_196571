399dbbe7bbcbdf917318048ce5a8686e81325882
alumno <alumno@iMac-V108-19.local>
2024-09-10 16:13:27 -0600

    Agregar maquina de estados y funcionalidad al pulsar algun boton con operacion vinculado

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -14,12 +14,13 @@ enum estados_de_la_calculadora{
     case mostrar_resultado
 }
 class ViewController: UIViewController {
-
+    var estado_actual : estados_de_la_calculadora = estados_de_la_calculadora.seleccionar_numeros
     @IBOutlet weak var texto_a_cambiar: UILabel!
     @IBOutlet weak var operaciones: UIButton!
     
     
     var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
+    var operacion_actual : String? = nil
     override func viewDidLoad() {
         
         
@@ -30,12 +31,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func que_hace_pushar_boton(_ sender: UIButton) {
+        if(estado_actual == estados_de_la_calculadora.seleccionar_numeros){
+            
         let text_a_añadir = botones_interfaz[(sender.restorationIdentifier ?? operaciones.restorationIdentifier) ?? "boton"]?.numero
                     
                     texto_a_cambiar.text = "\(texto_a_cambiar.text ?? "")\(text_a_añadir!)"
-
+        }
+        else if(estado_actual == estados_de_la_calculadora.escoger_operacion){
+            if let _mensajero: UIButton? = sender{
+                operacion_actual = botones_interfaz[_mensajero!.restorationIdentifier ?? "boton_0"]?.operacion
+            }
+            else{
+                operacion_actual = nil
+            }
+            //operacion_actual = botones_interfaz[(sender.restorationIdentifier)]!!.operacion
+        }
     }
     
+    @IBAction func boton_escoger_operacion_pulsado(_ sender: UIButton){
+        if(estado_actual == estados_de_la_calculadora.seleccionar_numeros){
+            estado_actual = estados_de_la_calculadora.escoger_operacion
+            dibujar_numeros_u_operaciones_en_interfaz()
+        }
+    }
     func inicializar_calculadora() -> Void{
         crear_arreglo_botones()
     }
@@ -44,6 +62,13 @@ class ViewController: UIViewController {
         botones_interfaz = IUBotonCalculadora.crear_arreglo_botones()
         
     }
-    
+    func dibujar_numeros_u_operaciones_en_interfaz(){
+        if(estado_actual == estados_de_la_calculadora.escoger_operacion){
+            
+        }
+        else if (estado_actual == estados_de_la_calculadora.seleccionar_numeros){
+            
+        }
+    }
 }
 
