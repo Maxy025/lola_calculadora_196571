e6f09fbe46b92b83a6f8601f56ac7e55c1fe3b29
alumno <alumno@iMac-V108-19.local>
2024-09-10 15:27:49 -0600

    Cambio de ubicacio el relleno del diccionario a la estructura IUBotonCalculadora

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -7,27 +7,33 @@
 
 import UIKit
 
+
+enum estados_de_la_calculadora{
+    case seleccionar_numeros
+    case escoger_operacion
+    case mostrar_resultado
+}
 class ViewController: UIViewController {
 
     @IBOutlet weak var texto_a_cambiar: UILabel!
-    //var botones_interfaz:   Array<IUBotonCalculadora> = []
     @IBOutlet weak var operaciones: UIButton!
+    
+    
     var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
     override func viewDidLoad() {
         
         
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         inicializar_calculadora()
+        
     }
 
     @IBAction func que_hace_pushar_boton(_ sender: UIButton) {
-        let text_a_añadir = botones_interfaz[(sender.restorationIdentifier ?? <#default value#>]?.numero
-                             texto_a_cambiar.text = "\(texto_a_cambiar.text)\(texto_a_añadir!)"
-        print(text_a_añadir)
-        // "hola mundo" + "boton7"
-        // "hola_mundoboton7"
+        let text_a_añadir = botones_interfaz[(sender.restorationIdentifier ?? operaciones.restorationIdentifier) ?? "boton"]?.numero
+                    
+                    texto_a_cambiar.text = "\(texto_a_cambiar.text ?? "")\(text_a_añadir!)"
+
     }
     
     func inicializar_calculadora() -> Void{
@@ -35,12 +41,8 @@ class ViewController: UIViewController {
     }
     
     func crear_arreglo_botones() -> Void{
-        for numero in 0...9{
-            botones_interfaz["boton_\(numero)"] = IUBotonCalculadora(
-                "boton_\(numero)",
-                operacion: "")
-            )
-        }
+        botones_interfaz = IUBotonCalculadora.crear_arreglo_botones()
+        
     }
     
 }
